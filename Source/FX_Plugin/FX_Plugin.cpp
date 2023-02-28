164d6d3e1fd2fd718d7ff88a2f15c0e5ee3a5037
FoxHoundcn <foxhoundcn@126.com>
2023-02-28 11:23:25 +0800

    delete
    delete

--- Source/FX_Plugin/FX_Plugin.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "FX_Plugin.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FX_Plugin, "FX_Plugin" );
